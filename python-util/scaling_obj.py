a9c81ff446f458c583ab2e0ad9cd72b8346cf5f9
Paolo Maldonado <jorpal_10_20@outlook.com>
2023-11-30 12:38:04 -0500

    Added python utility files for transforming and finding the vertex normals of an .obj file

---
+++ python-util/scaling_obj.py
@@ -0,0 +1,19 @@
+def rescale_obj(obj_path, obj_scaled_path, scale):
+    with open(obj_path, 'r') as source:
+        with open(obj_scaled_path, 'w') as target:
+            for line in source:
+                taget_line = line
+
+                if(line.startswith('v ')):
+                    coordinates = [float(coordinate) for coordinate in line.split(' ')[1:]]
+                    rescaled = [c*scale for c in coordinates]
+                    rescaled_as_str = " ".join([str(c) for c in rescaled])
+                    taget_line = f'v {rescaled_as_str}\n'
+
+                target.write(taget_line)
+
+def main():
+    rescale_obj("bunny.obj", "bunny_scaled.obj", 10.0)
+
+if __name__ == '__main__':
+    main()
